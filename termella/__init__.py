d6748290185c8ae7ed4ebf85ad58049bcc331e3a
codewithzaqar <zaqarstandoff@gmail.com>
2025-12-29 20:24:59 +0400

    Bump version to 0.0.9.dev0
    - Started v0.0.9 cycle: Object-Oriented Component System
    - Feature: Added termella.ui package
    - Feature: Added 'Widget' base class (implements __str__)
    - Feature: Added 'Label' component with markup support
    - Example: Added v009_dev0_test.py demonstrating stateful Label switching

--- termella/__init__.py
+++ termella/__init__.py
@@ -1,6 +1,6 @@
 """
 Termella - Rich text and beautiful formatting in the terminal.
-Version: 0.0.8
+Version: 0.0.9.dev0
 """
 
 from .printer import cprint, cinput
@@ -9,13 +9,15 @@ from .widgets import panel, progress_bar, table, Spinner, select, checkbox, tree
 from .live import Live
 from .markup import parse, print_tag, add_alias
 from .app import App
+from .ui import Widget, Label
 
-__version__ = "0.0.8"
+__version__ = "0.0.9.dev0"
 __all__ = [
     "cprint", "cinput", "Text", 
     "panel", "progress_bar", "table", "Spinner", 
     "select", "checkbox", "tree", "columns", "grid",
     "Live",
     "parse", "print_tag", "add_alias",
-    "App"
+    "App",
+    "Widget", "Label"
 ]
